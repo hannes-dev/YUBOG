c9d0628232e1000507cd54cd2013c2272860df06
Hannes <hannes-dev@mailbox.org>
2025-02-09 01:25:16 +0100

    add first implementation of bomb lib

--- BombLib.js
+++ BombLib.js
@@ -1,18 +1,13 @@
 export default class Bomb {
     strikes = 0;
 
-    constructor(modules, seconds, max_strikes) {
-        this.seconds = seconds;
-        this.max_strikes = max_strikes;
+    constructor(modules, seconds, strikes_allowed) {
+        this.total_time_ms = seconds * 1000;
+        this.strikes_allowed = strikes_allowed; // 1 more explodes
         console.log(this);
 
-        this.modules = [];
-        let i = 0;
-        for (let module of modules) {
-            this.modules[i] = module;
-            i++;
-        }
-
+        this.modules = {};
+        this.registerModules(modules)
         this.serial = this.generateSerial();
         this.batteries = this.generateBatteries();
         this.ports = this.generatePorts();
@@ -21,22 +16,81 @@ export default class Bomb {
     }
 
     start() {
+        this.waitForHello();
+    }
+
+    startGame() {
         const tick = new Event("tick");
-        this.start_time = Date.now();
+        this.start_time_ms = Date.now();
+        this.startModules();
+
+        // start ticking every ~100ms
         this.timer_interval = setInterval(() => {
-            this.time_left = this.seconds * 1000 - (Date.now() - this.start_time);
+            this.time_left =
+                this.total_time_ms - (Date.now() - this.start_time_ms);
             if (this.time_left <= 0) {
                 this.explode();
-                return
+                return;
             }
             window.dispatchEvent(tick);
         }, 100);
     }
 
+    // generate a UUID and prepare data
+    registerModules(modules) {
+        for (let module of modules) {
+            let module_id = self.crypto.randomUUID();
+            this.modules[module_id] = {
+                solved: false,
+                hello: false,
+                url: "",
+                frame: module,
+            }; 
+
+            module.onload = () => {
+                this.initModule(module_id);
+            }
+        }
+    }
+
+    // send init to modules, wait for hello
+    waitForHello() {
+        this.hello_timeout = setTimeout(() => {
+            // check if all modules replied
+            for (let module_id in this.modules) {
+                if (!this.modules[module_id].hello) {
+                    console.error("Not all modules replied hello", this.modules[module_id]);
+                    return;
+                }
+            }
+            // if all modules are ready, start game
+            this.startGame();
+        }, 5000)
+    }
+
+    initModule(module_id) {
+        this.sendToModule("init", module_id, {});
+    }
+
+    // send start signal to modules
+    startModules() {
+        for (let module_id in this.modules) {
+            this.sendToModule("start", module_id, {
+                total_modules: this.modules.length,
+                total_time_ms: this.total_time_ms,
+                start_time_ms: this.start_time_ms,
+                strikes_allowed: this.strikes_allowed,
+                seed: "ABCDEFG", // TODO
+                edgework: {}, // TODO
+            })
+        }
+    }
+
+
     strike() {
         this.strikes++;
         console.log(this.strikes);
-        if (this.strikes > this.max_strikes) {
+        if (this.strikes > this.strikes_allowed) {
             this.explode();
         }
     }
@@ -46,7 +100,7 @@ export default class Bomb {
         window.dispatchEvent(new Event("explode"));
     }
 
-    stop() {    
+    stop() {
         clearInterval(this.timer_interval);
     }
 
@@ -60,9 +114,23 @@ export default class Bomb {
     }
 
     handleMessage = (e) => {
+        if (e.data.type == "hello") {
+            // module replies, mark it as such
+            this.modules[e.data.module_id].hello = true;
+            // TODO if all modules work, start the game early (loading screen?)
+        }
         if (e.data.type == "strike") {
             this.strike();
         }
+    };
+
+    sendToModule(type, module_id, data) {
+        let message = {
+            type: type,
+            module_id: module_id,
+            data: data,
+        }
+        this.modules[module_id].frame.contentWindow.postMessage(message, "*");
     }
 
     generateSerial() {
@@ -70,10 +138,10 @@ export default class Bomb {
     }
 
     generateBatteries() {
-        return 3
+        return 3;
     }
 
     generatePorts() {
-        return ["Parallel", "Serial"]
+        return ["Parallel", "Serial"];
     }
 }
